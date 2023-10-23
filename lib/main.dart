d495e2642d14df2b73327818bf7a24626a70b2a3
khankotha <148752235+khankotha@users.noreply.github.com>
2023-10-23 14:28:11 +0600

    My First

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(myapp());
+}
+class myapp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home:app() ,);
+  }
+
+}
+class app extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('MyApp'),),
+      body: Center(child: Text('I am Kotha .App Developer  '),),
+
+
+    );
+  }
+
+}
